ea67ab227e99dfbad6cf2f2cc25d69e6c2465a1e
Heming233 <heming7159@163.com>
2021-10-09 01:25:44 +0800

    checkService.java checkServiceImpl.java moveService.java moveServiceImpl.java node.java panelService.java panelServiceImpl.java

--- src/main/java/entity/node.java
+++ src/main/java/entity/node.java
@@ -3,7 +3,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * @author Heming233
+ * @version v1.0
+ */
+
 @Data
 public class node {
-    private int depth;
+    private int[] position=new int[9];  //当前局面，元素编号为位置，元素数值为在该位置的数字
+    private int depth;  //当前深度；从初始位置走到当前位置的步数
+    private int evaluation; //从起始位置到目标位置的预估最小步数
+    private int misPosition;    //当前位置到目标位置的预估最小步数
+    private node parent;    //当前位置的父状态
 }
