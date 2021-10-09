5a1228c70755ea082d7d0b55c8fde3175c69f210
Heming233 <heming7159@163.com>
2021-10-10 01:10:22 +0800

    checkService.java

--- src/main/java/services/checkService.java
+++ src/main/java/services/checkService.java
@@ -13,15 +13,18 @@ import java.util.Arrays;
 public interface checkService {
 
     //检查当前状态是否到达目的状态
-    public boolean isCompleted(Node node,Node correct);
+    public boolean isCompleted(Node node, Node correct);
 
     //检查当前状态是否有解
     public boolean isSolvable(Node node);
 
     //检查能否移动
-    public boolean isMovable(int number,int[] position);
+    public boolean isMovable(int number, int[] position);
 
     //检查open表里有没有某节点
-    public boolean isExist(ArrayList<Node> open,Node node);
+    public boolean isExist(ArrayList<Node> open, Node node);
+
+   //检查该状态是不是上一个状态的父状态，感觉没必要，暂时放着
+    /*   public boolean isParent(ArrayList<Node> close,Node node);*/
 
 }
