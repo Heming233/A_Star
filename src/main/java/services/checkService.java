76fd9ed6a889b3e755013504771da6c8cf36346b
Heming233 <heming7159@163.com>
2021-10-10 00:54:55 +0800

    checkService.java checkServiceImpl.java

--- src/main/java/services/checkService.java
+++ src/main/java/services/checkService.java
@@ -2,6 +2,7 @@ package services;
 
 import entity.Node;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -17,5 +18,10 @@ public interface checkService {
     //检查当前状态是否有解
     public boolean isSolvable(Node node);
 
+    //检查能否移动
+    public boolean isMovable(int number,int[] position);
+
+    //检查open表里有没有某节点
+    public boolean isExist(ArrayList<Node> open,Node node);
 
 }
--- src/main/java/services/impl/checkServiceImpl.java
+++ src/main/java/services/impl/checkServiceImpl.java
@@ -3,6 +3,7 @@ package services.impl;
 import entity.Node;
 import services.checkService;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -39,4 +40,55 @@ public class checkServiceImpl implements checkService {
         }
     }
 
+    @Override
+    public boolean isMovable(int number,int[] position){
+        int[][] graph=new int[3][3];
+        //int flag=0;//用于确定选定的某个数四周有没有空格的标记
+        int n=0,i=0,j=0;
+
+        //将一维数组转移至二维数组
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                graph[i][j]=position[n++];
+            }
+        }
+        if(number>=0&&number<=2){
+            i=0;
+            j=number;
+        }
+        if(number>=3&&number<=5){
+            i=1;
+            j=number-3;
+        }
+        if(number>=6&&number<=8){
+            i=2;
+            j=number-6;
+        }
+
+        //判断position[number]四周有没有空格
+        if(graph[i-1][j]==0&&i-1>=0){
+            return true;
+        }
+        if(graph[i+1][j]==0&&i+1<=2){
+            return true;
+        }
+        if(graph[i][j-1]==0&&j-1>=0){
+            return true;
+        }
+        if(graph[i][j+1]==0&&j+1<=2){
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean isExist(ArrayList<Node> open,Node node){
+        if(open.contains(node)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
